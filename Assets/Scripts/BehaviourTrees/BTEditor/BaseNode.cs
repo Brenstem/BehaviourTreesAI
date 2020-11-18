e2dd1c7a0d8649b4f7b16d4da42db5c5427c9849
Brenstem <dane.nilsson@gmail.com>
2020-11-18 01:04:00 +0100

    Started work on editor window

---
+++ Assets/Scripts/BehaviourTrees/BTEditor/BaseNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseNode : ScriptableObject
+{
+    public Rect windowRect;
+    public string windowTitle;
+
+    public void DrawWindow()
+    {
+
+    }
+
+    public void DrawCurve()
+    {
+
+    }
+}
---
+++ Assets/Scripts/DanesUnityLibrary/Tree.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeNode<T>
+{
+    private T _datatype;
+    private TreeNode<T> parent;
+    private List<TreeNode<T>> children;
+
+    public TreeNode(T datatype, TreeNode<T> parent, List<TreeNode<T>> children)
+    {
+        _datatype = datatype;
+        this.parent = parent;
+        this.children = children;
+    }
+}
