3a88c6fb58b87361b4ddc50618e1659864ca37c0
Alexander Macri <alexander.m.1999@gmail.com>
2020-11-20 17:05:00 +0100

    Grid System

---
+++ Assets/Scripts/Grid/Test.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    private Grid grid;
+
+    void Start()
+    {
+        grid = new Grid(6, 4, 10f, new Vector3(-15, 10));
+        new Grid(4, 2, 5f, new Vector3(-5, -3));
+        new Grid(3, 8, 8f, new Vector3(60, -10));
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //print(GetMouseWorldPosition());
+            grid.AddToValue(GetMouseWorldPosition(), 1);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        { 
+            print("value is " + grid.GetValue(GetMouseWorldPosition()));
+        }
+        else if (Input.GetMouseButtonDown(2))
+        {
+            grid.SetValue(GetMouseWorldPosition(), 0);
+        }
+    }
+
+
+
+    Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0;
+        return vec;
+    }
+    Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
