467476533c8336f8a3d3a0e1d78342237092d3a0
Alexander Macri <alexander.m.1999@gmail.com>
2021-03-21 19:48:00 +0100

    började på nya spelaren

---
+++ Assets/Scripts/NewPlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewPlayerController : MonoBehaviour
+{
+    [SerializeField] LayerMask environmentLayerMask;
+    [SerializeField] float rotationSpeed;
+
+    //[SerializeField] float debugRadius;
+    //[SerializeField] GameObject debugObject;
+
+    DemoWeaponScript weapon;
+    NavMeshAgent agent;
+    Camera mainCamera;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        mainCamera = Camera.main;
+        weapon = GetComponentInChildren<DemoWeaponScript>();
+        //rotationCorutine = FaceDirection(transform.forward);
+
+        //agent.updateRotation = false;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+
+        Physics.Raycast(mousePosition, mainCamera.transform.forward, out hit, Mathf.Infinity, environmentLayerMask);
+
+        //transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //RaycastHit hit;
+
+            //Vector3 mousePosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+
+            if (Physics.Raycast(mousePosition, mainCamera.transform.forward, out hit, Mathf.Infinity, environmentLayerMask))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            weapon.FireWeapon();
+
+            //RaycastHit hit;
+
+            //Vector3 mousePosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+
+            //if (Physics.Raycast(mousePosition, mainCamera.transform.forward, out hit, Mathf.Infinity, environmentLayerMask))
+            //{
+            //    StopCoroutine(rotationCorutine);
+            //    rotationCorutine = FaceDirection((new Vector3(hit.point.x, transform.position.y, hit.point.z) - transform.position).normalized);
+            //    StartCoroutine(rotationCorutine);
+            //}
+        }
+    }
+}
