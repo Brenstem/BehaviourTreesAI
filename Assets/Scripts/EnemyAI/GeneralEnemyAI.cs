2a0c9991089550f0eb569adc89cb6a278c907bc2
Brenstem <dane.nilsson@gmail.com>
2021-03-31 18:12:39 +0200

    Finished first draft of unemotional Halo BT w general

--- Assets/Scripts/EnemyAI/GeneralEnemyAI.cs
+++ Assets/Scripts/EnemyAI/GeneralEnemyAI.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class GeneralEnemyAI : MonoBehaviour
+public class GeneralEnemyAI : BaseAI
 {
     [SerializeField] private GameObject soldier;
 
-    private DemoEnemyAI soldierAI;
-    private Health health;
-    private NavMeshAgent agent;
+    public DemoEnemyAI SoldierAI { get { return soldierAI; } }
 
-    void Awake()
+    private DemoEnemyAI soldierAI;
+
+    new void Awake()
     {
+        base.Awake();
+
         soldierAI = soldier.GetComponent<DemoEnemyAI>();
-        health = GetComponent<Health>();
-        agent = GetComponent<NavMeshAgent>();
+
+        behaviourTree.context.localData.Add<bool>(() => { return new BlackBoardProperty<bool>("TookDamage", false); });
+        behaviourTree.context.localData.Add<Vector3>(() => { return new BlackBoardProperty<Vector3>("positionToGoTo", Vector3.zero); });
     }
 
-    void Update()
+    private void Update()
     {
-        
+        float moveX = agent.velocity.x;
+        float moveZ = agent.velocity.z;
+
+        animator.SetFloat("moveZ", moveZ);
+        animator.SetFloat("moveX", moveX);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!GetComponent<Health>().isDead)
+        {
+            behaviourTree.topNodeInstance.Evaluate();
+        }
     }
 }
