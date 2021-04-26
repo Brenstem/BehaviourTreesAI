870d5674d46e74668e764144d92e9089566c07c7
Alexander Macri <alexander.m.1999@gmail.com>
2021-04-26 18:42:46 +0200

    fixat belly slide och bra shoot animation

--- Assets/AIBehaviours/PlayAnimationNode.cs
+++ Assets/AIBehaviours/PlayAnimationNode.cs
@@ -10,19 +10,19 @@ public class PlayAnimationNode : Action
     [SerializeField] private bool animBoolValue;
 
     [SerializeField] private bool waitForAnimation;
-    [SerializeField] private string[] animationNames;
+    [SerializeField] private string animationToWaitFor;
 
 
 
     private Animator animator;
-    private Timer timer;
+    //private Timer timer;
     private bool animationDone = false;
-
+    bool animationPlaying = false;
     public override void Construct()
     {
         _constructed = true;
         animator = context.owner.GetComponentInChildren<Animator>();
-        timer = new Timer(1f, () => { animationDone = true; timer.Reset(); });
+        //timer = new Timer(1f, () => { animationDone = true; timer.Reset(); });
     }
 
     public override NodeStates Evaluate()
@@ -37,46 +37,17 @@ public class PlayAnimationNode : Action
                 {
                     NodeState = NodeStates.SUCCESS;
                 }
-                //else if (animator.GetCurrentAnimatorStateInfo(0).IsName(animationName))
                 else
                 {
-                    animationDone = false;
-
-                    timer.DecrementTimer(Time.fixedDeltaTime);
-
-                    bool animPlaying = false;
-
-                    //TODO fixa detta med alla animationer och kolla om det funkar, i så fall gör så om ingen animation spelas så returnar den success
-
-                    foreach (string animationName in animationNames)
+                    if(animator.GetCurrentAnimatorStateInfo(0).IsName(animationToWaitFor))
                     {
-                        if (animator.GetCurrentAnimatorStateInfo(0).IsName(animationName))
-                        {
-                            animPlaying = true;
-                            Debug.Log(animationName);
-                            break;
-                        }
-                    }
-
-                    if (!animPlaying)
-                    {
-                        Debug.Log("no anim playing");
-                    }
-
-                    if (!animationDone)
-                    {
-                        NodeState = NodeStates.RUNNING;
+                        NodeState = NodeStates.SUCCESS;
                     }
                     else
                     {
-                        Debug.Log("Animation was done");
-                        NodeState = NodeStates.SUCCESS;
+                        NodeState = NodeStates.RUNNING;
                     }
                 }
-                //else
-                //{
-                //    NodeState = NodeStates.SUCCESS;
-                //}
             }
             else
             {
